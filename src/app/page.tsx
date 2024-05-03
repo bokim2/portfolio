6de8079f6e0d28027e330fa20ab3395dfdf61566
bokim2 <bokim.ce@gmail.com>
2024-05-03 12:32:00 -0700

    useEffect to make window work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,89 @@
-
-'use client';
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import Image from 'next/image';
+import styles from './page.module.css';
 import Lenis from '@studio-freight/lenis';
 import { useEffect, useState } from 'react';
 
-
-import Gallery from '../components/gallery'
+import Gallery from '../components/gallery';
 import { useMotionValue, useSpring } from 'framer-motion';
-
-
 
 const imagesAndBackgrounds = [
   {
-  name: 'moab1',
-  handle: 'moab_1',
-},
-{
-  name: 'sf1',
-  handle: 'sf_1',
-},
-{
-  name: 'tahoe1',
-  handle: 'tahoe_1',
-},
-]
+    name: 'moab1',
+    handle: 'moab_1',
+  },
+  {
+    name: 'sf1',
+    handle: 'sf_1',
+  },
+  {
+    name: 'tahoe1',
+    handle: 'tahoe_1',
+  },
+];
 
+export default function Home() {
+  // const [mousePosition, setMousePosition] =useState({ x: 0, y: 0 });
+  const springValues = {
+    stiffness: 150,
+    damping: 15,
+    mass: 0.1,
+  };
 
-  export default function Home() {
+  const [xPosition, setXPosition] = useState(0);
+  const [yPosition, setYPosition] = useState(0);
+  useEffect(() => {
+    setXPosition(window.innerWidth / 2 - (window.innerWidth * 0.25) / 2);
+    setYPosition(window.innerHeight / 2 - (window.innerHeight * 0.25) / 2);
+  }, []);
 
-    // const [mousePosition, setMousePosition] =useState({ x: 0, y: 0 });
-    const springValues = {
-      stiffness: 150,
-      damping: 15,
-      mass: .1,
-    }
-    const mousePosition = {
-      x: useSpring(window.innerWidth/2 - window.innerWidth * .25/2, springValues),
-      y: useSpring(window.innerHeight/2 - window.innerHeight * .25/2, springValues),
-    }
+  const mousePosition = {
+    x: useSpring(xPosition, springValues),
+    y: useSpring(yPosition, springValues),
+  };
 
-    useEffect(() => {
-      const lenis = new Lenis();
+  useEffect(() => {
+    const lenis = new Lenis();
 
-      // lenis.on('scroll', (e: Event) => {
-      //   // console.log(e);
-      // });
+    // lenis.on('scroll', (e: Event) => {
+    //   // console.log(e);
+    // });
 
-      function raf(time: number) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-      }
-
+    function raf(time: number) {
+      lenis.raf(time);
       requestAnimationFrame(raf);
-    }, []);
-
-
-    const mouseMove = (e: React.MouseEvent) => {
-      const { clientX, clientY } = e;
-      // console.log(clientX, clientY)
-      const centeredX = clientX - (window.innerWidth /2 * .25)
-      const centeredY = clientY - (window.innerWidth /2 * .30)
-
-      mousePosition.x.set(centeredX)
-      mousePosition.y.set(centeredY)
     }
-  
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  const mouseMove = (e: React.MouseEvent) => {
+    const { clientX, clientY } = e;
+    // console.log(clientX, clientY)
+    const centeredX = clientX - (window.innerWidth / 2) * 0.25;
+    const centeredY = clientY - (window.innerWidth / 2) * 0.3;
+
+    mousePosition.x.set(centeredX);
+    mousePosition.y.set(centeredY);
+  };
 
   return (
-    <main className={styles.main} >
+    <main className={styles.main}>
       {/* <div className={styles.container}>
         hello
       </div> */}
-    
+
       <div className={styles.description} onMouseMove={mouseMove}>
-       
-{
-  imagesAndBackgrounds.map(({handle, name}, i)=> {
-    return <Gallery handle={handle} name={name} mousePosition={mousePosition} key={i} />
-  })
-}
+        {imagesAndBackgrounds.map(({ handle, name }, i) => {
+          return (
+            <Gallery
+              handle={handle}
+              name={name}
+              mousePosition={mousePosition}
+              key={i}
+            />
+          );
+        })}
       </div>
     </main>
   );
