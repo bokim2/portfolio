37869cb27174c73761156e27593d19ea87e36629
bokim2 <bokim.ce@gmail.com>
2024-05-05 21:48:25 -0700

    layout

--- src/app/home-components/bio/bio.tsx
+++ src/app/home-components/bio/bio.tsx
@@ -6,38 +6,21 @@ import styles from './bio.module.scss';
 import Image from 'next/image';
 
 const imageVariants = {
-  initial: {filter: 'sepia(100%)'},
-  animate: {filter: 'sepia(0%)', transition: {delay: 1, duration: 1.5}},
-}
+  initial: { filter: 'sepia(100%)' },
+  animate: { filter: 'sepia(0%)', transition: { delay: 1, duration: 1.5 } },
+};
 
 export default function Bio() {
   return (
     <section className={styles.bio}>
-        <div className={styles.bioAbout}>
-      <div className={styles.bioTitleContainer}>
-        <h1 className={styles.bioName}>Bo Kim</h1>
-        <h2 className={styles.bioRole}>Software Engineer & Scientist</h2>
-
-          <p>
-            With ten years of experience in biotech, I have recently
-            transitioned into software engineering, motivated by a passion for
-            digital innovation and problem-solving.
-            <br />
-            My background in scientific research and process automation gives me
-            a strong foundation for understanding complex systems and data
-            analysis. While my current focus is on the JavaScript ecosystem,
-            including tools like React and Node.js, I am also looking to expand
-            my technical repertoire by learning new technologies and programming
-            languages. My goal is to continually enhance my capabilities to
-            craft innovative software solutions.
-          </p>
-        </div>
-
-  <motion.div className={styles.backgroundImgContainer}
-        variants={imageVariants}
-        initial='initial'
-        animate='animate'>
-        <Image
+      <div className={styles.headshotImgOuterContainer}>
+        <motion.div
+          className={styles.headshotImgContainer}
+          variants={imageVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <Image
             className={`${styles.headshotImg} `}
             src="/images/intro/headshot.png"
             alt="background"
@@ -47,8 +30,28 @@ export default function Bio() {
             priority
             // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
-
         </motion.div>
+      </div>
+
+      <div className={styles.bioAbout}>
+        <div className={styles.bioTitleContainer}>
+          <h1 className={styles.bioName}>Bo Kim</h1>
+          <h2 className={styles.bioRole}>Software Engineer & Scientist</h2>
+
+          <p>
+          As a software engineer with a passion for digital innovation and problem-solving, I transitioned from a decade-long career in biotech, where I worked in scientific research and process automation. 
+            <br />
+            <br />
+           While my current focus is in the JavaScript ecosystem,
+            including tools like React and Node.js, I am also looking to expand
+            my technical skills by learning new technologies and programming
+            languages. 
+            <br />
+            <br />
+            My goal is to continually enhance my capabilities to
+            craft innovative software solutions.
+          </p>
+        </div>
       </div>
     </section>
   );
