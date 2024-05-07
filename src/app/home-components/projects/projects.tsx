583a2fcba265e93c1a680defc762270dbf83e1a8
bokim2 <bokim.ce@gmail.com>
2024-05-06 17:11:41 -0700

    tech stack list

--- src/app/home-components/projects/projects.tsx
+++ src/app/home-components/projects/projects.tsx
@@ -23,6 +23,21 @@ export default function Projects() {
               hand. Securely log in with Auth0 (Google or GitHub) to observe and
               save your data.
             </p>
+
+            <div className={styles.projectTechContainer}>
+              <ul>
+                <li>React</li>
+                <li>React Router</li>
+                <li>React Query</li>
+                <li>Redux Toolkit</li>
+                <li>Styled Components</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>PostgresSQL</li>
+                <li>React ChartJS</li>
+                <li>Heroku</li>
+              </ul>
+            </div>
           </div>
 
           <div className={styles.projectImageContainer}>
@@ -40,13 +55,25 @@ export default function Projects() {
           <div className={styles.projectText}>
             <h2 className={styles.projectTitle}>Rediscope 2.0</h2>
             <p>
-              Web app for recording and visualizing scientific data. Capture
-              your strain information, start your flasks, and view previous
-              growth curves to plan your sample times. Create sample schedules
-              easily, use the dilution calculator - no need to calculate by
-              hand. Securely log in with Auth0 (Google or GitHub) to observe and
-              save your data.
+              An open source Redis database metrics visualization tool. Use the
+              customizable data collection intervals and set max data points for
+              enhanced data resolution. Dockerized and deployed using AWS.
             </p>
+
+            <div className={styles.projectTechContainer}>
+              <ul>
+                <li>React</li>
+                <li>React Router</li>
+                <li>Styled Components</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>PostgresSQL</li>
+                <li>React ChartJS</li>
+                <li>Node-Redis</li>
+                <li>Docker</li>
+                <li>AWS</li>
+              </ul>
+            </div>
           </div>
 
           <div className={styles.projectImageContainer}>
