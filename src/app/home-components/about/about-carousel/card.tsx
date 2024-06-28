8a6d25ee759d84e83588c46d0f3e05777bad4b8a
bokim2 <bokim.ce@gmail.com>
2024-06-28 12:47:43 -0700

    card image overrides have been created

--- src/app/home-components/about/about-carousel/card.tsx
+++ src/app/home-components/about/about-carousel/card.tsx
@@ -1,5 +1,5 @@
 import Image, { StaticImageData } from 'next/image';
-import { useState } from 'react';
+import { CSSProperties, useState } from 'react';
 
 import { motion } from 'framer-motion';
 import { BsFillCaretLeftFill, BsFillCaretRightFill } from 'react-icons/bs';
@@ -44,48 +44,59 @@ type TSlide = {
   equipment?: string;
   pic1: StaticImageData;
   pic2?: StaticImageData | undefined;
-  text?: JSX.Element;
+  pic1StyleOverrides?: CSSProperties;
+  pic2StyleOverrides?: CSSProperties;
+  text?: string[];
 };
 
 export const CARD_DATA = [
- [ {
-    title: 'the journey up to this point',
-    subtitle: 'biotech',
-    equipment: '',
-    pic1: genencorFun,
-    pic2: genencor2,
-    pic1StyleOverrides: '',
-    pic2StyleOverrides: '',
-    text: (
-      <>
-        My primary area of specialization was upstream process development, aka
-        working with bioreactors, often referred to as &quot;fermentation&quot;,
+  [
+    {
+      title: 'the journey up to this point',
+      subtitle: 'biotech',
+      equipment: '',
+      pic1: genencorFun,
+      pic2: genencor2,
+      pic1StyleOverrides: {
+        // transform: 'scale(2)', 
+        // backgroundColor: 'red', 
+        // opacity: .2
+      },
+      pic2StyleOverrides: {
+                // transform: 'scale(2)', 
+        // backgroundColor: 'red', 
+        // opacity: .2
+      },
+      text: [
+        `My primary area of specialization was upstream process development, aka
+        working with bioreactors, often referred to as "fermentation",
         a term originating from the historical use of microorganisms for the
-        production of alcoholic beverages.
-        <br />
-      </>
-    ),
-  },
-  {
-    title: 'the journey up to this point2',
-    subtitle: 'biotech2',
-    equipment: '',
-    pic1: biostatB,
-    pic2: mfcs,
-    text: (
-      <>
-        My experience involved cultivating genetically modified microbes to
-        produce therapeutic proteins and enzymes. Additionally, I was involved
-        in the production of molecules such as squalene, an oil found in shark
-        liver used in cosmetics, and Rebaudioside M (rebM), a zero-calorie
-        sweetener naturally derived from the stevia plant.
-      </>
-    ),
-  },
-  { pic1: biomek1, pic2: biomek2 },
-  { pic1: dasgip, pic2: dasgipSw },
-  { pic1: ambr250, pic2: ambr250Sw },
-  { pic1: suf, pic2: sufProfile },]
+        production of alcoholic beverages.`,
+        `test, next paragraph lorem25`,
+        `more test`,
+      ],
+    },
+    {
+      title: 'the journey up to this point2',
+      subtitle: 'biotech2',
+      equipment: '',
+      pic1: biostatB,
+      pic2: mfcs,
+      text: [
+        `
+          My experience involved cultivating genetically modified microbes to
+          produce therapeutic proteins and enzymes. Additionally, I was involved
+          in the production of molecules such as squalene, an oil found in shark
+          liver used in cosmetics, and Rebaudioside M (rebM), a zero-calorie
+          sweetener naturally derived from the stevia plant.
+`,
+      ],
+    },
+    { pic1: biomek1, pic2: biomek2 },
+    { pic1: dasgip, pic2: dasgipSw },
+    { pic1: ambr250, pic2: ambr250Sw },
+    { pic1: suf, pic2: sufProfile },
+  ],
 ] as TSlide[][];
 
 // export const ABOUT_ME = [
@@ -176,24 +187,34 @@ export default function Card({ item, i }: TCard) {
   return (
     <div className={styles.card}>
       <div className={styles.cardTitles}>
-        <h3>{item?.[activeIndex]?.title}</h3>
-        <p className={`xlarge ${styles.cardSubtitle}`}>{item?.[activeIndex]?.subtitle}</p>
+        <h3>{item?.[activeIndex]?.title ?? ''}</h3>
+        <p className={`xlarge ${styles.cardSubtitle}`}>
+          {item?.[activeIndex]?.subtitle ?? ''}
+        </p>
       </div>
 
       <motion.div className={styles.cardInner}>
         <div className={styles.cardColumn}>
           <div className={`${styles.cardTextAndSecondImage}`}>
-            <p className={`large ${styles.cardText}`}>
-              {item?.[activeIndex]?.text}
-            </p>
+            <div className={`${styles.cardTextContainer}`}>
+              {item?.[activeIndex]?.text?.map((singleParagraph, i) => (
+                <>
+                  <p key={i} className={`large`}>
+                    {singleParagraph}
+                  </p>
+                  {i < (item?.[activeIndex]?.text?.length ?? 0) - 1 && <br />}
+                </>
+              ))}
+            </div>
 
-            {/* second pi&& vs ??cture */}
+            {/* second pic */}
             {item?.[activeIndex]?.pic2 ? (
               <div className={styles.cardImageContainerSecondary}>
                 <Image
                   src={item?.[activeIndex]?.pic2 as StaticImageData}
                   alt={item?.[activeIndex]?.title as string}
                   layout="fill"
+                  style={item?.[activeIndex]?.pic2StyleOverrides ?? {}}
                 />
               </div>
             ) : (
@@ -210,6 +231,7 @@ export default function Card({ item, i }: TCard) {
                 alt={item?.[activeIndex]?.title as string}
                 layout="fill"
                 //   objectFit="cover"
+                style={item?.[activeIndex]?.pic1StyleOverrides ?? {}}
                 key={i}
               />
               <CardControls handleIndexChange={handleIndexChange} />
