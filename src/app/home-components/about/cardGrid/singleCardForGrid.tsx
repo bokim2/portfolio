4f4d88e4eb995c1131e8033fd7ef800bd50e548d
bokim2 <bokim.ce@gmail.com>
2024-07-14 12:53:48 -0700

    working on about section

--- src/app/home-components/about/cardGrid/singleCardForGrid.tsx
+++ src/app/home-components/about/cardGrid/singleCardForGrid.tsx
@@ -16,8 +16,10 @@ import styles from '../about-carousel/aboutCarousel.module.scss';
 import genencorFun from '@/../public/images/carousel/1.jpg';
 import genencor2 from '@/../public/images/progressive/genencor3.jpg';
 import egyptWine from '@/../public/images/carousel/egyptWine.jpg';
+import background from '@/../public/images/carousel/background.jpg';
 
 import biostatB from '@/../public/images/carousel/biostatB.jpg';
+import biostatBY from '@/../public/images/carousel/biostatBY.jpg';
 import mfcs from '@/../public/images/carousel/mfcs.jpg';
 
 import biomek1 from '@/../public/images/carousel/biomek1.jpg';
@@ -36,6 +38,8 @@ import ambr1 from '@/../public/images/carousel/ambr1.jpg';
 import ambr2 from '@/../public/images/carousel/ambr2.jpg';
 
 import suf from '@/../public/images/carousel/suf.jpg';
+import suf300 from '@/../public/images/carousel/suf300.jpg';
+import sufDeflated from '@/../public/images/carousel/sufDeflated.jpg';
 import suf2 from '@/../public/images/carousel/suf2.jpg';
 import sufProfile from '@/../public/images/carousel/sufProfile.jpg';
 import sufSw2 from '@/../public/images/carousel/sufSw2.jpg';
@@ -44,6 +48,8 @@ import MF_thermo_suf from '@/../public/images/carousel/MF_thermo_suf.jpg';
 // Current CARD
 import codesmith from '@/../public/images/carousel/current/codesmith.jpg';
 import codesmith2 from '@/../public/images/carousel/current/codesmith2.jpg';
+import rediscopeSH from '@/../public/images/carousel/current/rediscopeSH.jpg';
+import rediscopeZ from '@/../public/images/carousel/current/rediscopeZ.jpg';
 
 import aws from '@/../public/images/carousel/current/aws.jpg';
 import awsVPN from '@/../public/images/carousel/current/awsVPN.jpg';
@@ -52,6 +58,7 @@ import ambr15 from '@/../public/images/carousel/current/ambr15.jpg';
 import dna from '@/../public/images/carousel/current/dna.jpg';
 
 import interest3d from '@/../public/images/carousel/current/interest3d.jpg';
+import spotfireEX from '@/../public/images/carousel/current/spotfireEX.jpg';
 
 import commercialScale from '@/../public/images/carousel/current/commercialScale.jpg';
 import manyBioreactors from '@/../public/images/carousel/current/manyBioreactors.jpg';
@@ -59,6 +66,11 @@ import manyBioreactors from '@/../public/images/carousel/current/manyBioreactors
 import mosaic from '@/../public/images/carousel/current/mosaic.jpg';
 import bubbleDark from '@/../public/images/carousel/current/bubbleDark.jpg';
 import genencor4 from '@/../public/images/progressive/genencor4.jpg';
+
+import ambr250BM from '@/../public/images/carousel/ambr250BM.jpg';
+import fermAmyrisBM from '@/../public/images/carousel/fermAmyrisBM.jpg';
+import industrialBioBM from '@/../public/images/carousel/industrialBioBM.jpg';
+import operizBM from '@/../public/images/carousel/operizBM.jpg';
 
 import CardControls from '../about-carousel/cardControls';
 
@@ -90,16 +102,18 @@ export const SINGLE_CARD_DATA = [
       title: 'the journey up to this point',
       subtitle: 'biotech',
       equipment: '',
-      pic1: genencorFun,
-      pic2: genencor2,
+      pic1: fermAmyrisBM,
+      pic2: background,
       // pic3: egyptWine,
       pic1StyleOverrides: {
         // transform: 'scale(2)',
         // backgroundColor: 'red',
         // opacity: .2
+        objectFit: 'contain',
       },
       pic2StyleOverrides: {
         // transform: 'scale(2)',
+        objectFit: 'contain',
         // backgroundColor: 'red',
         // opacity: .2
       },
@@ -151,17 +165,37 @@ export const SINGLE_CARD_DATA = [
       title: 'Evolution of Bioreactors',
       subtitle: 'Satorius Biostat B',
       equipment: '',
-      pic1: biostatB,
+      pic1: biostatBY,
       pic2: mfcs,
-      pic1ImageCaption: 'standard 2L bioreactor - not the original, but close',
+      pic1ImageCaption: 'standard 2L bioreactor',
       pic2ImageCaption: 'MFCS software - difficult to set up or modify recipes',
       text: [<></>],
     },
 
-    { pic1: dasgip, pic2: dasgipSw },
+    {
+      pic1: dasgip,
+      pic2: dasgipSw,
+      pic1StyleOverrides: {
+        // objectFit: 'contain',
+        filter: 'brightness(1.05)',
+        transform: 'scale(1.25)',
+
+        objectFit: 'contain',
+      },
+      pic2StyleOverrides: {},
+    },
     { pic1: biolector1, pic2: MF_biolector_feed_profile },
-    { pic1: ambr250, pic2: ambr1 },
-    { pic1: suf2, pic2: sufSw2 },
+    { pic1: ambr250, pic2: operizBM },
+    {
+      pic1: sufDeflated,
+      pic2: sufProfile,
+      pic1StyleOverrides: {
+        filter: 'brightness(1.5)',
+      },
+      pic2StyleOverrides: {
+        filter: 'brightness(1.2)',
+      },
+    },
   ],
 
   // second card
@@ -171,7 +205,7 @@ export const SINGLE_CARD_DATA = [
       subtitle: 'Codesmith',
       equipment: '',
       pic1: codesmith,
-      pic2: codesmith2,
+      pic2: rediscopeZ,
       // pic3: egyptWine,
       pic1StyleOverrides: {
         objectFit: 'contain',
@@ -180,8 +214,9 @@ export const SINGLE_CARD_DATA = [
         // opacity: .2
       },
       pic2StyleOverrides: {
-        objectFit: 'contain',
-        // transform: 'scale(2)',
+        // transform: 'scale(1.5)',
+        objectPosition: 'center right',
+        // objectFit: 'contain',
         // backgroundColor: 'red',
         // opacity: .2
       },
@@ -234,180 +269,12 @@ export const SINGLE_CARD_DATA = [
       ],
     },
 
-    // {
-    //   title: 'Self-learning',
-    //   subtitle: '',
-    //   equipment: '',
-    //   pic1: codesmith,
-    //   pic2: codesmith2,
-    //   // pic3: egyptWine,
-    //   pic1StyleOverrides: {
-    //     objectFit: 'contain',
-    //     // transform: 'scale(2)',
-    //     // backgroundColor: 'red',
-    //     // opacity: .2
-    //   },
-    //   pic2StyleOverrides: {
-    //     objectFit: 'contain',
-    //     // transform: 'scale(2)',
-    //     // backgroundColor: 'red',
-    //     // opacity: .2
-    //   },
-    //   pic1ImageCaption: 'remote instruction.',
-    //   pic2ImageCaption: '3 months',
-    //   text: [
-    //     <>
-    //       started looking at backend, started learning python for algos. but
-    //       eventually backed off to work on frontend to build a portfolio. was
-    //       surprised to find that i really enjoyed frontend, and stayed there for
-    //       a while.
-    //       {/* <em>
-    //         <b> full stack</b>
-    //       </em> */}
-    //     </>,
-    //     <></>,
-    //   ],
-    // },
-    // {
-    //   title: 'Why software engineering',
-    //   subtitle: '',
-    //   equipment: '',
-    //   pic1: codesmith,
-    //   pic2: codesmith2,
-    //   // pic3: egyptWine,
-    //   pic1StyleOverrides: {
-    //     objectFit: 'contain',
-    //     // transform: 'scale(2)',
-    //     // backgroundColor: 'red',
-    //     // opacity: .2
-    //   },
-    //   pic2StyleOverrides: {
-    //     objectFit: 'contain',
-    //     // transform: 'scale(2)',
-    //     // backgroundColor: 'red',
-    //     // opacity: .2
-    //   },
-    //   pic1ImageCaption: 'remote instruction.',
-    //   pic2ImageCaption: '3 months',
-    //   text: [
-    //     <>
-    //       my favorite part of working in biotech in the bay area was getting to
-    //       work with cutting edge equipment and software and also seeing the
-    //       iteration and progression of these tools over time.
-    //       {/* <em>
-    //         <b> full stack</b>
-    //       </em> */}
-    //     </>,
-    //     <>
-    //       After a decade in biotech, I have worked with most of the industry
-    //       standard tools in my area of specialization and developments in this
-    //       area is slower (game changing new tools every couple of years) than
-    //       you would find in a industry like tech.
-    //     </>,
-    //   ],
-    // },
-
-    // {
-    //   title: 'bioreactors - their role I',
-    //   subtitle: 'a testing platform',
-    //   equipment: '',
-    //   pic1: manyBioreactors,
-    //   pic2: dna,
-    //   // pic3: egyptWine,
-    //   pic1StyleOverrides: {
-    //     objectFit: 'contain',
-    //     // transform: 'scale(2)',
-    //     // backgroundColor: 'red',
-    //     // opacity: .2
-    //   },
-    //   pic2StyleOverrides: {
-    //     objectFit: 'contain',
-    //     // transform: 'scale(2)',
-    //     // backgroundColor: 'red',
-    //     // opacity: .2
-    //   },
-    //   pic1ImageCaption: 'remote instruction.',
-    //   pic2ImageCaption: '3 months',
-    //   text: [
-    //     <>
-    //       Over time, there has been more and more emphasis on throughput and
-    //       miniaturization in the fermentation space. This is because a large
-    //       part of bioreactor usage is for screening strains / variants with the
-    //       same fermentation recipe.
-    //       {/* <em>
-    //         <b> full stack</b>
-    //       </em> */}
-    //     </>,
-    //     <>
-    //       In my opinion, a lot of the focus in biotech is more in molecular
-    //       biology / strain engineering these days. After all, you can not make a
-    //       bad strain produce a lot of product, no matter what you do in
-    //       fermentation.
-    //     </>,
-    //   ],
-    // },
-
-    // {
-    //   title: 'bioreactors - their role II',
-    //   subtitle: 'a industrial scale model',
-    //   equipment: '',
-    //   pic1: commercialScale,
-    //   pic2: codesmith2,
-    //   // pic3: egyptWine,
-    //   pic1StyleOverrides: {
-    //     objectFit: 'contain',
-    //     // transform: 'scale(2)',
-    //     // backgroundColor: 'red',
-    //     // opacity: .2
-    //   },
-    //   pic2StyleOverrides: {
-    //     objectFit: 'contain',
-    //     // transform: 'scale(2)',
-    //     // backgroundColor: 'red',
-    //     // opacity: .2
-    //   },
-    //   pic1ImageCaption: 'remote instruction.',
-    //   pic2ImageCaption: '3 months',
-    //   text: [
-    //     <>
-    //       serious constraints due to what is possible at industrial scale.
-    //       Commercial scale plants often have poorer control over conditions than
-    //       bench scale, ex: poorer mixing, temperature control.
-    //       {/* <em>
-    //         <b> full stack</b>
-    //       </em> */}
-    //     </>,
-    //     <>
-    //       cost has taken center stage. biotech processes are expensive!
-    //       experimenting with media composition is limited, whatever goes in
-    //       costs $$, or may need to be removed at the end of the process (or can
-    //       interfere with purification). they say 90% of biotech production cost
-    //       is in the recovery / filtration process.
-    //     </>,
-    //   ],
-    // },
-    // {
-    //   title: 'Evolution of Bioreactors',
-    //   subtitle: 'Satorius Biostat B',
-    //   equipment: '',
-    //   pic1: biostatB,
-    //   pic2: mfcs,
-    //   pic1ImageCaption: 'standard 2L bioreactor - not the original, but close',
-    //   pic2ImageCaption: 'MFCS software - difficult to set up or modify',
-    //   text: [<></>],
-    // },
-
-    // { pic1: dasgip, pic2: dasgipSw },
-    // { pic1: biolector1, pic2: biolector2 },
-    // { pic1: ambr250, pic2: ambr250Sw },
-    // { pic1: suf, pic2: sufSw1 },
- 
     {
       title: 'Areas of Interest',
       subtitle: '',
       equipment: '',
       pic1: interest3d,
-      pic2: codesmith2,
+      pic2: spotfireEX,
       // pic3: egyptWine,
       pic1StyleOverrides: {
         // objectFit: 'contain',
@@ -484,7 +351,7 @@ export const SINGLE_CARD_DATA = [
       },
       pic2StyleOverrides: {
         objectPosition: 'top',
-        filter: 'brightness(1.2)'
+        filter: 'brightness(1.05)',
         // objectFit: 'contain',
         // transform: 'scale(2)',
         // backgroundColor: 'red',
@@ -494,29 +361,13 @@ export const SINGLE_CARD_DATA = [
       text: [
         <>
           I love an open, inclusive, and engaging work culture. I worked in the
-          San Francisco Bay Area during my time in biotech and enjoyed it. I hope to be based
-          in California, Colorado, or Washington.
+          San Francisco Bay Area during my time in biotech and enjoyed it. I
+          hope to be based in California, Colorado, or Washington.
         </>,
         <></>,
         ,
       ],
     },
-
-    // {
-    //   title: 'Evolution of Bioreactors',
-    //   subtitle: 'Satorius Biostat B',
-    //   equipment: '',
-    //   pic1: biostatB,
-    //   pic2: mfcs,
-    //   pic1ImageCaption: 'standard 2L bioreactor - not the original, but close',
-    //   pic2ImageCaption: 'MFCS software - difficult to set up or modify',
-    //   text: [<></>],
-    // },
-
-    // { pic1: dasgip, pic2: dasgipSw },
-    // { pic1: biolector1, pic2: biolector2 },
-    // { pic1: ambr250, pic2: ambr250Sw },
-    // { pic1: suf, pic2: sufSw1 },
   ],
   [],
 ] as TSlide[][];
@@ -889,3 +740,187 @@ export default function SingleCardForGrid({
     </div>
   );
 }
+
+// {
+//   title: 'Self-learning',
+//   subtitle: '',
+//   equipment: '',
+//   pic1: codesmith,
+//   pic2: codesmith2,
+//   // pic3: egyptWine,
+//   pic1StyleOverrides: {
+//     objectFit: 'contain',
+//     // transform: 'scale(2)',
+//     // backgroundColor: 'red',
+//     // opacity: .2
+//   },
+//   pic2StyleOverrides: {
+//     objectFit: 'contain',
+//     // transform: 'scale(2)',
+//     // backgroundColor: 'red',
+//     // opacity: .2
+//   },
+//   pic1ImageCaption: 'remote instruction.',
+//   pic2ImageCaption: '3 months',
+//   text: [
+//     <>
+//       started looking at backend, started learning python for algos. but
+//       eventually backed off to work on frontend to build a portfolio. was
+//       surprised to find that i really enjoyed frontend, and stayed there for
+//       a while.
+//       {/* <em>
+//         <b> full stack</b>
+//       </em> */}
+//     </>,
+//     <></>,
+//   ],
+// },
+// {
+//   title: 'Why software engineering',
+//   subtitle: '',
+//   equipment: '',
+//   pic1: codesmith,
+//   pic2: codesmith2,
+//   // pic3: egyptWine,
+//   pic1StyleOverrides: {
+//     objectFit: 'contain',
+//     // transform: 'scale(2)',
+//     // backgroundColor: 'red',
+//     // opacity: .2
+//   },
+//   pic2StyleOverrides: {
+//     objectFit: 'contain',
+//     // transform: 'scale(2)',
+//     // backgroundColor: 'red',
+//     // opacity: .2
+//   },
+//   pic1ImageCaption: 'remote instruction.',
+//   pic2ImageCaption: '3 months',
+//   text: [
+//     <>
+//       my favorite part of working in biotech in the bay area was getting to
+//       work with cutting edge equipment and software and also seeing the
+//       iteration and progression of these tools over time.
+//       {/* <em>
+//         <b> full stack</b>
+//       </em> */}
+//     </>,
+//     <>
+//       After a decade in biotech, I have worked with most of the industry
+//       standard tools in my area of specialization and developments in this
+//       area is slower (game changing new tools every couple of years) than
+//       you would find in a industry like tech.
+//     </>,
+//   ],
+// },
+
+// {
+//   title: 'bioreactors - their role I',
+//   subtitle: 'a testing platform',
+//   equipment: '',
+//   pic1: manyBioreactors,
+//   pic2: dna,
+//   // pic3: egyptWine,
+//   pic1StyleOverrides: {
+//     objectFit: 'contain',
+//     // transform: 'scale(2)',
+//     // backgroundColor: 'red',
+//     // opacity: .2
+//   },
+//   pic2StyleOverrides: {
+//     objectFit: 'contain',
+//     // transform: 'scale(2)',
+//     // backgroundColor: 'red',
+//     // opacity: .2
+//   },
+//   pic1ImageCaption: 'remote instruction.',
+//   pic2ImageCaption: '3 months',
+//   text: [
+//     <>
+//       Over time, there has been more and more emphasis on throughput and
+//       miniaturization in the fermentation space. This is because a large
+//       part of bioreactor usage is for screening strains / variants with the
+//       same fermentation recipe.
+//       {/* <em>
+//         <b> full stack</b>
+//       </em> */}
+//     </>,
+//     <>
+//       In my opinion, a lot of the focus in biotech is more in molecular
+//       biology / strain engineering these days. After all, you can not make a
+//       bad strain produce a lot of product, no matter what you do in
+//       fermentation.
+//     </>,
+//   ],
+// },
+
+// {
+//   title: 'bioreactors - their role II',
+//   subtitle: 'a industrial scale model',
+//   equipment: '',
+//   pic1: commercialScale,
+//   pic2: codesmith2,
+//   // pic3: egyptWine,
+//   pic1StyleOverrides: {
+//     objectFit: 'contain',
+//     // transform: 'scale(2)',
+//     // backgroundColor: 'red',
+//     // opacity: .2
+//   },
+//   pic2StyleOverrides: {
+//     objectFit: 'contain',
+//     // transform: 'scale(2)',
+//     // backgroundColor: 'red',
+//     // opacity: .2
+//   },
+//   pic1ImageCaption: 'remote instruction.',
+//   pic2ImageCaption: '3 months',
+//   text: [
+//     <>
+//       serious constraints due to what is possible at industrial scale.
+//       Commercial scale plants often have poorer control over conditions than
+//       bench scale, ex: poorer mixing, temperature control.
+//       {/* <em>
+//         <b> full stack</b>
+//       </em> */}
+//     </>,
+//     <>
+//       cost has taken center stage. biotech processes are expensive!
+//       experimenting with media composition is limited, whatever goes in
+//       costs $$, or may need to be removed at the end of the process (or can
+//       interfere with purification). they say 90% of biotech production cost
+//       is in the recovery / filtration process.
+//     </>,
+//   ],
+// },
+// {
+//   title: 'Evolution of Bioreactors',
+//   subtitle: 'Satorius Biostat B',
+//   equipment: '',
+//   pic1: biostatB,
+//   pic2: mfcs,
+//   pic1ImageCaption: 'standard 2L bioreactor - not the original, but close',
+//   pic2ImageCaption: 'MFCS software - difficult to set up or modify',
+//   text: [<></>],
+// },
+
+// { pic1: dasgip, pic2: dasgipSw },
+// { pic1: biolector1, pic2: biolector2 },
+// { pic1: ambr250, pic2: ambr250Sw },
+// { pic1: suf, pic2: sufSw1 },
+
+// {
+//   title: 'Evolution of Bioreactors',
+//   subtitle: 'Satorius Biostat B',
+//   equipment: '',
+//   pic1: biostatB,
+//   pic2: mfcs,
+//   pic1ImageCaption: 'standard 2L bioreactor - not the original, but close',
+//   pic2ImageCaption: 'MFCS software - difficult to set up or modify',
+//   text: [<></>],
+// },
+
+// { pic1: dasgip, pic2: dasgipSw },
+// { pic1: biolector1, pic2: biolector2 },
+// { pic1: ambr250, pic2: ambr250Sw },
+// { pic1: suf, pic2: sufSw1 },
